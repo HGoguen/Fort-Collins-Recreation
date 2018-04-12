efbe22105ffe8e8a973ab88e6e49eb3b18fd47a2
Hannah <hegoguen@student.cccs.edu>
2018-04-12 12:56:24 -0600

    Basemap
    Imagary with Labels basemap. With future code for layer.

--- js/FinalProjectCode.js
+++ js/FinalProjectCode.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var map = L.map("map").setView([37.75, -122.23], 10);
-
-  L.esri.basemapLayer("Topographic").addTo(map);
-    
+  var map = L.map("map").setView([40.5, -105.0844], 11);
+  
+  L.esri.basemapLayer('Imagery').addTo(map);
+  L.esri.basemapLayer('ImageryLabels').addTo(map);
+  //L.esri.featureLayer({
+   // url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0'
+  //}).addTo(map);
 });
