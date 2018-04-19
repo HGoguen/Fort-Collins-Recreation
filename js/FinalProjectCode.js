ed58c1b364f7fb7e2298719bf4eeecb4434df15d
HGoguen <35549599+HGoguen@users.noreply.github.com>
2018-04-18 18:18:17 -0600

    HostedFeatureLayer
    Added jhosted feature layer of bike trails in fort collins.

--- js/FinalProjectCode.js
+++ js/FinalProjectCode.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var map = L.map("map").setView([40.5, -105.0844], 11);
+  var map = L.map("map").setView([40.55, -105.0844], 12);
   
   L.esri.basemapLayer('Imagery').addTo(map);
   L.esri.basemapLayer('ImageryLabels').addTo(map);
-  //L.esri.featureLayer({
-   // url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0'
-  //}).addTo(map);
+  L.esri.featureLayer({
+    url: 'http://services.arcgis.com/Vr4pJuhEJB9F4bUA/arcgis/rest/services/TrailsNpaths/FeatureServer/0'
+  }).addTo(map);
 });
