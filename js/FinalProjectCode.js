630f2ae26fa46f76db986cafe1438980ab6b489d
Hannah <hegoguen@student.cccs.edu>
2018-04-25 16:02:07 -0600

    Popups added
    nature area not working; trail popup not working

--- js/FinalProjectCode.js
+++ js/FinalProjectCode.js
@@ -10,6 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var pathLayer = L.esri.featureLayer({
         url: 'http://services.arcgis.com/Vr4pJuhEJB9F4bUA/arcgis/rest/services/TrailsNpaths/FeatureServer/0'
     }).addTo(map);
+    
+     var popupTemplate = "<h3>{NAME}</h3> Bike use: {BIKEUSE} <br> Horse Use: {HORSEUSE} <br> Surface Type: {CONSTRUCTI}";
+
+      pathLayer.bindPopup(function(e){
+        return L.Util.template(popupTemplate, e.feature.properties)
+      });
 
     //My Feature Layer
     var naturalFort = L.esri.featureLayer({
@@ -20,6 +26,11 @@ document.addEventListener('DOMContentLoaded', function() {
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/FortCollinsNatureAreas/FeatureServer/0'      
     }).addTo(map);
 
+    var popupTemplate = "<h3>{PARKNAME}</h3> This is a {PARKTYPE} <br> Parking Onstreet: {PRKNGONST} <br> Parking Off Street: {PRKNGOFFST}";
+
+      parksFort.bindPopup(function(e){
+        return L.Util.template(popupTemplate, e.feature.properties)
+      });
 
     // create the geocoding control and add it to the map
     var searchControl = L.esri.Geocoding.geosearch().addTo(map);
